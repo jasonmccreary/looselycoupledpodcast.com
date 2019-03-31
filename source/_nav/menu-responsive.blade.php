7db6b214692b214a518ac752bb59677a39d194e8
Jess Archer <jess@jessarcher.com>
2019-03-31 15:37:22 +1000

    Remove "About" page

--- source/_nav/menu-responsive.blade.php
+++ source/_nav/menu-responsive.blade.php
@@ -9,13 +9,6 @@
         </li>
         <li class="pl-4">
             <a
-                title="{{ $page->siteName }} About"
-                href="/about"
-                class="nav-menu__item hover:text-blue-500 {{ $page->isActive('/about') ? 'active text-blue-500' : '' }}"
-            >About</a>
-        </li>
-        <li class="pl-4">
-            <a
                 title="{{ $page->siteName }} Contact"
                 href="/contact"
                 class="nav-menu__item hover:text-blue-500 {{ $page->isActive('/contact') ? 'active text-blue-500' : '' }}"
--- source/_nav/menu.blade.php
+++ source/_nav/menu.blade.php
@@ -4,11 +4,6 @@
         Blog
     </a>
 
-    <a title="{{ $page->siteName }} About" href="/about"
-        class="ml-6 text-gray-700 hover:text-blue-600 {{ $page->isActive('/about') ? 'active text-blue-600' : '' }}">
-        About
-    </a>
-
     <a title="{{ $page->siteName }} Contact" href="/contact"
         class="ml-6 text-gray-700 hover:text-blue-600 {{ $page->isActive('/contact') ? 'active text-blue-600' : '' }}">
         Contact
