6d166940effc795d60e4925c322ddad66845f062
Jason McCreary <jason@pureconcepts.net>
2019-05-20 22:38:22 -0400

    Use description as excerpt

--- config.php
+++ config.php
@@ -21,7 +21,7 @@ return [
         return Datetime::createFromFormat('U', $page->date);
     },
     'getExcerpt' => function ($page, $length = 255) {
-        $content = $page->excerpt ?? $page->getContent();
+        $content = $page->description ?? $page->getContent();
         $cleaned = strip_tags(
             preg_replace(['/<pre>[\w\W]*?<\/pre>/', '/<h\d>[\w\W]*?<\/h\d>/'], '', $content),
             '<code>'
