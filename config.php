61a6adaf45220e7056ae0160ae99e8cbd4c5b517
Jess Archer <jess@jessarcher.com>
2019-07-03 18:37:36 +1000

    Add podcast badges for Apple and Spotify

--- config.php
+++ config.php
@@ -6,6 +6,10 @@ return [
     'siteName' => 'The BaseCode Podcast',
     'siteDescription' => 'A podcast of development related topics hosted from opposite sides of the planet.',
     'siteAuthor' => 'JMac and Jess',
+    'podcastBadgeLinks' => [
+        'apple' => 'https://podcasts.apple.com/us/podcast/the-basecode-podcast/id1466110887',
+        'spotify' => 'https://open.spotify.com/show/1fZHlOQkgJkmPbYpchfVDr',
+    ],
 
     // collections
     'collections' => [
