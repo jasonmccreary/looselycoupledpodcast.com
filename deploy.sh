cfdc7f5d3bc54d17ca4dbf71b948a9146e847216
Jason McCreary <jason@pureconcepts.net>
2020-03-10 14:47:24 -0400

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 vendor/bin/jigsaw build production
 npm run production
-rsync -v -rz --checksum --delete build_production/ shift-web:/var/www/basecodefieldguide.com/public/podcast
+rsync -v -rz --checksum --delete build_production/ web:/var/www/basecodefieldguide.com/public/podcast
 rm -rf build_production
